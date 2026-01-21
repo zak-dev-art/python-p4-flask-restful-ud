9a233cf19b6e495ddcbd655711164ec5f94b36a8
zak-dev-art <zachndungu861@gmail.com>
2026-01-21 07:25:44 +0300

    Add PATCH and DELETE methods to NewsletterByID resource

--- server/app.py
+++ server/app.py
@@ -80,6 +80,40 @@ class NewsletterByID(Resource):
 
         return response
 
+    def patch(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+
+        db.session.add(record)
+        db.session.commit()
+
+        response_dict = record.to_dict()
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
+    def delete(self, id):
+
+        record = Newsletter.query.filter_by(id=id).first()
+
+        db.session.delete(record)
+        db.session.commit()
+
+        response_dict = {"message": "record successfully deleted"}
+
+        response = make_response(
+            response_dict,
+            200
+        )
+
+        return response
+
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 
